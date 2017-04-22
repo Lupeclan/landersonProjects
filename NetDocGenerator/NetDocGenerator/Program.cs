7eda63a2c8799dbb0bf0731ab75f2cd7c16bcde7
Lucas Anderson <lupeclan@gmail.com>
2017-04-22 19:53:44 -0400

    Added NetDocGenerator under source control

---
+++ NetDocGenerator/NetDocGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using NetDocGeneratorCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDocGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new DocGenerator();
+            generator.Generate();
+        }
+    }
+}
---
+++ NetDocGenerator/NetDocGeneratorCore/DocGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDocGeneratorCore
+{
+    public class DocGenerator
+    {
+        private string InputFileName = "input.cs";
+
+        public DocGenerator()
+        {
+            var fileDetails = File.ReadAllLines(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), InputFileName));
+        }
+
+        public void Generate()
+        {
+
+        }
+    }
+}
